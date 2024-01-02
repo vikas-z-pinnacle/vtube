f61d1ad8882d8a81b9e009cbe6a66b70a96780d7
vikas-z-pinnacle <vikas.zilpe@pinnacle.in>
2024-01-02 11:37:31 +0530

    Mongo DB connection

---
+++ README.md
@@ -0,0 +1,8 @@
+# Mongo DB password - replace special character
+
+: (Colon) is %3A
+@ (At symbol) is %40
+/ (Forward slash) is %2F
++ (Plus sign) is %2B
+= (Equal sign) is %3D
+& (Ampersand) is %26
---
+++ backend/src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`\n Mongo DB connected !! DB Host: ${connectionInstance.connection.host}`)
+    } catch (error) {
+        console.log("Mongo DB connection error ", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
