c4cc27eef5d6a9b5484d6b8407f9625a12ef6019
vikas-z-pinnacle <vikas.zilpe@pinnacle.in>
2024-01-09 13:38:53 +0530

    New files created

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose, { Schema } from "mongoose";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema(
     {
@@ -16,8 +16,8 @@ const userSchema = new Schema(
             type: String,
             required: true,
             unique: true,
-            lowercase: true,
-            trim: true
+            lowecase: true,
+            trim: true,
         },
         fullname: {
             type: String,
@@ -26,11 +26,11 @@ const userSchema = new Schema(
             index: true
         },
         avatar: {
-            type: String, //cloudinary url
-            required: true
+            type: String, // cloudinary url
+            required: true,
         },
         coverImage: {
-            type: String, //cloudinary url
+            type: String, // cloudinary url
         },
         watchHistory: [
             {
@@ -40,30 +40,31 @@ const userSchema = new Schema(
         ],
         password: {
             type: String,
-            required: [true, "Password is required"]
+            required: [true, 'Password is required']
         },
         refreshToken: {
             type: String
         }
+
     },
     {
         timestamps: true
     }
-);
+)
 
 userSchema.pre("save", async function (next) {
-    if(!this.isModified("password")) return next();
-    
+    if (!this.isModified("password")) return next();
+
     this.password = await bcrypt.hash(this.password, 10)
     next()
-});
+})
 
 userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
 }
 
 userSchema.methods.generateAccessToken = function () {
-    jwt.sign(
+    return jwt.sign(
         {
             _id: this._id,
             email: this.email,
@@ -76,11 +77,11 @@ userSchema.methods.generateAccessToken = function () {
         }
     )
 }
-
 userSchema.methods.generateRefreshToken = function () {
-    jwt.sign(
+    return jwt.sign(
         {
-            _id: this._id
+            _id: this._id,
+
         },
         process.env.REFRESH_TOKEN_SECRET,
         {
@@ -89,4 +90,4 @@ userSchema.methods.generateRefreshToken = function () {
     )
 }
 
-export const User = mongoose.model("User", userSchema);
+export const User = mongoose.model("User", userSchema)
