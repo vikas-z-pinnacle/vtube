292a26e12d04a1aec7f10cae5975d6a3fcc5bd08
vikas-z-pinnacle <vikas.zilpe@pinnacle.in>
2024-01-11 17:57:32 +0530

    API updated

--- backend/src/controllers/healthcheck.controller.js
+++ backend/src/controllers/healthcheck.controller.js
@@ -1,6 +1,6 @@
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 
 
 const healthcheck = asyncHandler(async (req, res) => {
@@ -9,5 +9,4 @@ const healthcheck = asyncHandler(async (req, res) => {
 
 export {
     healthcheck
-    }
-    
+}
--- backend/src/controllers/like.controller.js
+++ backend/src/controllers/like.controller.js
@@ -1,22 +1,22 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Like} from "../models/like.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose, { isValidObjectId } from "mongoose"
+import { Like } from "../models/like.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const {videoId} = req.params
+    const { videoId } = req.params
     //TODO: toggle like on video
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
-    const {commentId} = req.params
+    const { commentId } = req.params
     //TODO: toggle like on comment
 
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
-    const {tweetId} = req.params
+    const { tweetId } = req.params
     //TODO: toggle like on tweet
 }
 )
