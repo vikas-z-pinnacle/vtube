00ed3e87ff131d214edf20ad005660469b86aedd
vikas-z-pinnacle <vikas.zilpe@pinnacle.in>
2024-01-16 18:05:35 +0530

    Comment Module Completed

--- backend/src/controllers/comment.controller.js
+++ backend/src/controllers/comment.controller.js
@@ -5,22 +5,123 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 
 const getVideoComments = asyncHandler(async (req, res) => {
-    //TODO: get all comments for a video
-    const { videoId } = req.params
-    const { page = 1, limit = 10 } = req.query
+    try {
+        const { videoId } = req.params
+        const { page = 1, limit = 10 } = req.query
+
+        const matchCriteria = {};
+        if(videoId){
+            matchCriteria.video = videoId
+        }
+
+        const comments = await Comment.aggregatePaginate([
+            { $match : matchCriteria }
+        ], { page, limit })
+
+        return res.status(200).json(
+            new ApiResponse(200, comments, "Comments fetched successfully")
+        )
+    } catch (error) {
+        throw new ApiError(500, "Error fetching comments: " + error.message ) 
+    }
 
 })
 
 const addComment = asyncHandler(async (req, res) => {
-    // TODO: add a comment to a video
+    try {
+        const { content } = req.body
+        const { videoId } = req.params
+
+        if(content.trim() === ""){
+            throw new ApiError(400, "Content is required")
+        }
+
+        if(!mongoose.Types.ObjectId.isValid(videoId)){
+            return res.status(400).json(
+                new ApiResponse(400, videoId, "Invalid videoId format")
+            )
+        }
+
+        //create new twweet
+        const newComment = await Comment.create({
+            content: content,
+            video: videoId,
+            owner: req.user._id  
+        })
+
+        return res.status(201).json(
+            new ApiResponse(200, newComment, "Comment created successfully")
+        )
+    } catch (error) {
+        throw new ApiError(500, "Error creating comment: " + error.message)
+    }
 })
 
 const updateComment = asyncHandler(async (req, res) => {
-    // TODO: update a comment
+    try {
+        const { content } = req.body
+        const { commentId } = req.params
+
+        if(!mongoose.Types.ObjectId.isValid(commentId)){
+            return res.status(400).json(
+                new ApiResponse(400, commentId, "Invalid commentId format")
+            )
+        }
+
+        const commentData = await Comment.findById(commentId).populate('owner', 'username');
+
+        if(!commentData){
+            return res.status(404).json(
+                new ApiResponse(404, commentData, "Comment not found")
+            )
+        }
+
+        const updateValues = {};
+
+        if(content){
+            updateValues.content = content;
+        }
+
+        const updatedComment = await Comment.findByIdAndUpdate(
+            commentId,
+            updateValues,
+            {new: true, runValidators: true}
+        ).populate("owner", "username");
+
+        return res.status(200).json(
+            new ApiResponse(200, updatedComment, "Comment updated successfully")
+        )
+    } catch (error) {
+        throw new ApiError(500, "Error updating comment: " + error.message )
+    }
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
-    // TODO: delete a comment
+    try {
+        const { commentId } = req.params
+
+        if(!mongoose.Types.ObjectId.isValid(commentId)){
+            return res.status(400).json(
+                new ApiResponse(400, commentId, "Invalid commentId format")
+            )
+        }
+
+        const commentToDelete = await Comment.findById(commentId);
+
+        if(!commentToDelete){
+            return res.status(404).json(
+                new ApiResponse(404, commentToDelete, "Comment not found")
+            )
+        }
+
+        await Comment.deleteOne({ _id: commentId });
+
+        return res.status(200).json(
+            new ApiResponse(200, {}, "Comment deleted successfully")
+        )
+    } catch (error) {
+        throw new ApiError(500, "Error deleting comment: " + error.message)
+    }
 })
 
 export {
